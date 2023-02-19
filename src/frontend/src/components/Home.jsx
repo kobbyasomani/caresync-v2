7e40019d89040a85537e98bc296f521ae2dbb1ed
Kobby Asomani <42306027+kobbyasomani@users.noreply.github.com>
2023-02-19 14:38:45 +0800

    feat: add controlled inputs and front-end validation for login form

--- src/frontend/src/components/Home.jsx
+++ src/frontend/src/components/Home.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
+
 export default function Home() {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+        errors: []
+    });
+
+    /**
+     * Handle controlled form input.
+     */
+    function handleInput(event) {
+        setForm(prev => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.value,
+                errors: []
+            }
+        });
+    }
+
+    /**
+     * Validate and submit the login form.
+     */
+    function handleLogin(event) {
+        event.preventDefault();
+        // Make sure fields are not empty
+        ["email", "password"].forEach(fieldValue => {
+            if (!form[fieldValue]) {
+                setForm(prev => {
+                    return {
+                        ...prev,
+                        errors: [`${fieldValue} cannot be blank.\n`]
+                    }
+                });
+            }
+        });
+        if (form.errors) return;
+        // If there are no errors submit the form
+    }
+
     return (
         <>
             <h1>CareSync</h1>
             <h2>Easy care work scheduling and shift notes.</h2>
             <section>
-                <form>
+                <form onSubmit={handleLogin}>
                     <fieldset>
                         <legend>Sign in</legend>
                         <label>Email address
@@ -12,20 +53,28 @@ export default function Home() {
                                 id="email"
                                 type="email"
                                 name="email"
-                                placeholder="your@provider.com" />
+                                placeholder="your@provider.com"
+                                required
+                                value={form.email}
+                                onChange={handleInput} />
                         </label>
                         <label>Password
                             <input
                                 id="password"
                                 type="password"
                                 name="password"
-                                placeholder="**********" />
+                                placeholder="**********"
+                                required
+                                value={form.password}
+                                onChange={handleInput} />
                         </label>
-                        <button>Log in</button>
+                        <button>
+                            Log in
+                        </button>
                     </fieldset>
                 </form>
+                <div id="form-error">{form.errors}</div>
             </section>
-
         </>
     )
 }
