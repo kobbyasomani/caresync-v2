f607f1c7f5a734b4ca8dba388d362b252fbd6d44
Kobby Asomani <42306027+kobbyasomani@users.noreply.github.com>
2023-03-03 16:36:13 +0800

    fix: modal/drawer no longer in already-open state when navigating away from error route

--- src/frontend/src/views/Error.jsx
+++ src/frontend/src/views/Error.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import {
-    useRouteError,
-    Link
-} from "react-router-dom";
+import { React, useEffect } from 'react';
+import { useRouteError, Link } from "react-router-dom";
 
+import { useModalContext } from '../utils/modalUtils';
 import NavBar from "../components/root/NavBar";
 import Footer from "../components/root/Footer";
 import { ButtonPrimary } from "../components/root/Buttons";
@@ -12,6 +10,21 @@ import { Typography, Box, Container } from "@mui/material"
 
 export default function Error() {
     const error = useRouteError();
+    const { modalDispatch } = useModalContext();
+
+    /* Close open modal/drawer if user hits the error route to
+prevent them being already-open when navigating to another view */
+    useEffect(() => {
+        modalDispatch({
+            type: "close",
+            data: "modal"
+        });
+        modalDispatch({
+            type: "close",
+            data: "drawer"
+        });
+    }, [modalDispatch]);
+
     return (
         <Box sx={{
             display: "flex",
