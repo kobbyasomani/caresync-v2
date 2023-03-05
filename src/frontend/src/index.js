cd538bab64e560051b4f3df555ce065a1e5bfe3e
David Tjomsland <dtjomslandjr@gmail.com>
2023-03-05 22:44:49 +0800

    New env variable

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import "./styles/index.css"
 import axios from "axios";
 
-axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
