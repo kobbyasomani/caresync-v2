4e719e6a1f94b4fe8ebbd2f2417b6fcda8b67da2
David Tjomsland <dtjomslandjr@gmail.com>
2023-03-05 18:53:06 +0800

    added new seed data

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import "./styles/index.css"
 import axios from "axios";
 
-axios.defaults.baseURL = `https://t3a2-b-production.up.railway.app`;
+axios.defaults.baseURL = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_BACKEND_PORT}`;
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
