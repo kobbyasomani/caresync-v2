660842bb3240dd8511157d6f928c10e2172639b1
Kobby Asomani <42306027+kobbyasomani@users.noreply.github.com>
2023-03-05 20:16:01 +0800

    feat: add tooltip to switch patient button

--- src/frontend/src/components/SelectedPatient.jsx
+++ src/frontend/src/components/SelectedPatient.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../utils/globalUtils";
 import { useModalContext } from "../utils/modalUtils";
 
-import { Card, CardContent, Avatar, CardMedia, Typography, CardActionArea } from "@mui/material"
+import { Card, CardContent, Avatar, CardMedia, Typography, CardActionArea, Tooltip } from "@mui/material"
 import PersonIcon from '@mui/icons-material/Person';
 import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import { Theme as theme } from "../styles/Theme";
@@ -39,30 +39,32 @@ const Patient = () => {
     }, [dispatch, modalDispatch, store.selectedPatient]);
 
     return (
-        <Card variant="outlined" id={_id} className="patient selected">
-            <CardActionArea
-                onClick={switchPatient}
-                sx={{
-                    display: "grid",
-                    gridTemplate: "repeat(2, auto) / auto 1fr",
-                    // columnGap: "1rem",
-                    // alignItems: "center"
-                }}>
-                <CardMedia sx={{ px: 2 }}>
-                    <div className="icon">
-                        <Avatar sx={{ backgroundColor: theme.palette.primary.light, height: "2rem", width: "2rem" }}>
-                            <PersonIcon fontSize="medium" />
-                        </Avatar>
-                    </div>
-                </CardMedia>
-                <CardContent sx={{ display: "flex", pl: 0 }}>
-                    <Typography variant="body1" className="name" sx={{ fontWeight: "600" }}>
-                        {firstName} {lastName}
-                    </Typography>
-                    <SwapHorizIcon sx={{ ml: "auto" }} />
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <Tooltip title="Switch Patient" placement="right" >
+            <Card variant="outlined" id={_id} className="patient selected">
+                <CardActionArea
+                    onClick={switchPatient}
+                    sx={{
+                        display: "grid",
+                        gridTemplate: "repeat(2, auto) / auto 1fr",
+                        // columnGap: "1rem",
+                        // alignItems: "center"
+                    }}>
+                    <CardMedia sx={{ px: 2 }}>
+                        <div className="icon">
+                            <Avatar sx={{ backgroundColor: theme.palette.primary.light, height: "2rem", width: "2rem" }}>
+                                <PersonIcon fontSize="medium" />
+                            </Avatar>
+                        </div>
+                    </CardMedia>
+                    <CardContent sx={{ display: "flex", pl: 0 }}>
+                        <Typography variant="body1" className="name" sx={{ fontWeight: "600" }}>
+                            {firstName} {lastName}
+                        </Typography>
+                        <SwapHorizIcon sx={{ ml: "auto" }} />
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Tooltip>
     );
 }
 
