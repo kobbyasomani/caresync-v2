2b7c91c953fd8c5b3575a72e82b60b7ee2294dbd
David Tjomsland <dtjomslandjr@gmail.com>
2023-03-05 15:04:01 +0800

    fixed env variables

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import "./styles/index.css"
 import axios from "axios";
 
-axios.defaults.baseURL = `${process.env.REACT_APP_BASE_URL}:${process.env.REACT_APP_BACKEND_PORT}`;
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
