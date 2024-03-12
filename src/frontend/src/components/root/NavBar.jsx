5f6b6eaf223d84b509669ae09e05a7a151273aac
Kobby Asomani <42306027+kobbyasomani@users.noreply.github.com>
2024-03-12 11:23:51 +0800

    Display IconButtons instead of Buttons in NavBar auth area at xs screen size

--- src/frontend/src/components/root/NavBar.jsx
+++ src/frontend/src/components/root/NavBar.jsx
@@ -1,15 +1,15 @@
 import { useCallback, React } from "react";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
+
 import { useGlobalContext } from "../../utils/globalUtils";
 import IconSmall from "../logo/IconSmall";
 
 import {
-    AppBar,
-    Toolbar,
-    Button,
-    Container, Box,
-    Typography, Stack,
-    Tooltip
+    AppBar, Toolbar,
+    Button, IconButton,
+    Container, Stack,
+    Typography, Tooltip,
+    useMediaQuery, useTheme
 } from "@mui/material";
 import LogoutIcon from '@mui/icons-material/Logout';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -17,6 +17,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 const NavBar = () => {
     const { store, dispatch } = useGlobalContext();
     const navigate = useNavigate();
+    const theme = useTheme();
+    const xsScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
     const handleLogout = useCallback(() => {
         dispatch({
@@ -49,7 +51,6 @@ const NavBar = () => {
                                 : "/"}>
                         <IconSmall />
                     </Button>
-
                     {navItems.map((item, index) => (
                         <Button
                             component={RouterLink}
@@ -60,28 +61,50 @@ const NavBar = () => {
                         </Button>
                     ))}
                     {store.isAuth ? (
-                        <Stack direction="row" gap={2} sx={{ ml: "auto" }}>
-                            <Stack direction="row" gap={0.5} sx={{ alignItems: "center" }}>
-                                {/* // TODO: Add login credential editing modal for easier re-access of demo accounts */}
-                                <AccountCircleIcon />
-                                <Typography>
-                                    {store.user.firstName}
-                                </Typography>
-                            </Stack>
-                            <Tooltip position="bottom" title="Log out" sx={{ display: { xs: "none", md: "flex" } }}>
-                                <Button id="logout"
-                                    startIcon={<LogoutIcon />}
-                                    color="inherit"
-                                    aria-label="log out"
-                                    size="small"
-                                    onClick={handleLogout}
-                                >
-                                    <Box sx={{ display: { xs: "none", md: "inline" } }}>
-                                        Log out
-                                    </Box>
-                                </Button>
+                        <Stack direction="row" gap={1} sx={{ ml: "auto" }}>
+                            {/* // TODO: Add login credential editing modal for easier re-access of demo accounts */}
+                            <Tooltip title="My account">
+                                {xsScreen ? (
+                                    <IconButton id="my-account"
+                                        color="inherit"
+                                        aria-label="my account"
+                                    >
+                                        <AccountCircleIcon />
+                                    </IconButton>
+                                ) : (
+                                    <Button id="my-account"
+                                        startIcon={<AccountCircleIcon />}
+                                        color="inherit"
+                                        aria-label="my account"
+                                    >
+                                        <Typography textTransform="capitalize">
+                                            {store.user.firstName}
+                                        </Typography>
+                                    </Button>
+                                )}
                             </Tooltip>
-
+                            <Tooltip position="bottom" title="Log out">
+                                {xsScreen ? (
+                                    <IconButton id="log-out"
+                                        color="inherit"
+                                        aria-label="log out"
+                                        onClick={handleLogout}
+                                    >
+                                        <LogoutIcon />
+                                    </IconButton>
+                                ) : (
+                                    <Button id="log-out"
+                                        startIcon={<LogoutIcon />}
+                                        color="inherit"
+                                        aria-label="log out"
+                                        onClick={handleLogout}
+                                    >
+                                        <Typography textTransform="capitalize">
+                                            Log out
+                                        </Typography>
+                                    </Button>
+                                )}
+                            </Tooltip>
                         </Stack>
                     ) : (
                         null
