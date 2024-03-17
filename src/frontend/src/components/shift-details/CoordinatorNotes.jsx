abe908919cd4104eee8eff1236662eae181d728b
Kobby Asomani <42306027+kobbyasomani@users.noreply.github.com>
2024-03-17 14:30:21 +0800

    Add a button to edit notes to Coordinator Notes panel
    The user can quickly access the Edit Shift Form and change the coordinator
    notes if they are the coordinator for the client and the shift is pending
    or in-progress.

--- src/frontend/src/components/shift-details/CoordinatorNotes.jsx
+++ src/frontend/src/components/shift-details/CoordinatorNotes.jsx
@@ -4,27 +4,42 @@ import { useNavigate } from "react-router-dom";
 import { useGlobalContext } from "../../utils/globalUtils";
 import { useModalContext } from "../../utils/modalUtils";
 import { getUserName } from "../../utils/apiUtils";
-import { ButtonPrimary } from "../root/Buttons";
+import { ButtonPrimary, ButtonSecondary } from "../root/Buttons";
 import Carer from "../Carer";
 import Loader from "../logo/Loader";
 
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Stack, useMediaQuery, useTheme } from "@mui/material";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
 
 const CoordinatorNotes = (props) => {
     const { store } = useGlobalContext();
     const { modalDispatch } = useModalContext();
     const navigate = useNavigate();
+    const { shiftUtils } = props;
 
     const [isLoading, setIsLoading] = useState(true);
     const [coordinator, setCoordinator] = useState({});
 
-    const openCareTeamList = useCallback(() => {
+    const theme = useTheme();
+    const xsScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const handleOpenCareTeamList = useCallback(() => {
         modalDispatch({
             type: "open",
             data: "modal",
             id: "care-team-list"
         });
         navigate("/calendar/care-team");
+    }, [modalDispatch, navigate]);
+
+    const handleEditShift = useCallback(() => {
+        modalDispatch({
+            type: "open",
+            data: "modal",
+            id: "edit-shift"
+        });
+        navigate("/calendar/edit-shift")
     }, [modalDispatch, navigate]);
 
     const renderContent = () => {
@@ -56,7 +71,7 @@ const CoordinatorNotes = (props) => {
 
     const ShiftCoordinatorDetails = () => {
         return (
-            <Carer carer={coordinator} onClick={openCareTeamList} />
+            <Carer carer={coordinator} onClick={handleOpenCareTeamList} />
         );
     };
 
@@ -68,9 +83,19 @@ const CoordinatorNotes = (props) => {
             </Box>
             <Box mt={2}>
                 <ShiftCoordinatorDetails />
-                <ButtonPrimary onClick={openCareTeamList}>
-                    View full care team
-                </ButtonPrimary>
+                <Stack direction="row" gap={2} justifyContent="center" mt={2}>
+                    {store.selectedClient.isCoordinator
+                        && (shiftUtils.isPending || shiftUtils.isInProgress) ?
+                        <ButtonPrimary onClick={handleEditShift} startIcon={<AssignmentIcon />}
+                            sx={{ margin: "0" }}>
+                            Edit notes
+                        </ButtonPrimary>
+                        : null}
+                    <ButtonSecondary onClick={handleOpenCareTeamList} startIcon={<Diversity3Icon />}
+                        sx={{ margin: "0" }}>
+                        {xsScreen ? "Care team" : "View full care team"}
+                    </ButtonSecondary>
+                </Stack>
             </Box>
         </>
     )
